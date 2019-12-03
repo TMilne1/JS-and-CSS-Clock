2a6df4d573b4d38d851343b66f3156994544f50a
tmilne1 <tmilne1@gmail.com>
2019-12-02 23:40:11 -0500

    fixed the hands from 'reversing' around the clock to get from 354 degrees to 0 degrees

--- main.js
+++ main.js
@@ -13,8 +13,24 @@ setDate = () =>{
     const minHand = document.querySelector('.min-hand')
     const secondHand = document.querySelector('.second-hand');
 
+    if (hourDegrees == 360 || minDegrees == 360 || secDegrees == 360) {
+        hourHand.style.transition = `none`
+        minHand.style.transition = `none`
+        secondHand.style.transition = `none`
+    }
+
     hourHand.style.transform = `rotate(${hourDegrees}deg)`;
     minHand.style.transform = `rotate(${minDegrees}deg)`;
     secondHand.style.transform = `rotate(${secDegrees}deg)`;
+
+    if (hourHand.style.transition == `none` || minHand.style.transition == `none` || secondHand.style.transition == `none`){
+        hourHand.style.transition= `all 0.5s`
+        minHand.style.transition = `all 0.5s`
+        secondHand.style.transition = `all 0.5s`
+
+    
+    }
+
+
 }
 setInterval(setDate, 1000);
